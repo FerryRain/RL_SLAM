8d5cba81ed2858f8a33481e302beb35957d7c0c3
kenny <kenny0326@qq.com>
2023-01-12 23:19:46 -0800

    preprocess_vector

--- RL_train/preprocess_vector.py
+++ RL_train/preprocess_vector.py
@@ -7,65 +7,63 @@
 # Description：
 """
 import argparse
-from pathlib import Path
+import sys
 
+import cv2
 import numpy as np
 import rospy
-from std_msgs.msg import String
-from sensor_msgs.msg import Image, CameraInfo
-import message_filters
-import cv2
-from cv_bridge import CvBridge, CvBridgeError
-from hwt_data.msg import Hwt_ht_basic
 import torch
-import torch.backends.cudnn as cudnn
+from cv_bridge import CvBridge
+from hwt_data.msg import Hwt_ht_basic
 from numpy import random
-from Environment_preprocessing.test_yolo import mainloop, args_set, plot_frame
+from sensor_msgs.msg import Image
 
+from Environment_preprocessing.test_yolo import plot_frame
 from Thirdparty.yolov7.models.experimental import attempt_load
-from Thirdparty.yolov7.utils.datasets import LoadStreams, LoadImages
-from Thirdparty.yolov7.utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
-    scale_coords, xyxy2xywh, set_logging, increment_path
-from Thirdparty.yolov7.utils.plots import plot_one_box
-from Thirdparty.yolov7.utils.torch_utils import select_device, load_classifier, time_synchronized, TracedModel
 from Thirdparty.yolov7.utils.datasets import letterbox
+from Thirdparty.yolov7.utils.general import check_img_size, non_max_suppression, \
+    scale_coords, set_logging
+from Thirdparty.yolov7.utils.plots import plot_one_box
+from Thirdparty.yolov7.utils.torch_utils import select_device, time_synchronized, TracedModel
 
-import sys
 sys.path.append('../Thirdparty/yolov7')
 
 global color_image, depth_image, basic
 global old_img_b, old_img_h, old_img_w
 
+"""
+-------------------------------------
+    # Args Init
+-------------------------------------
+"""
 def args_set():
     """
     Initialization Parameter
     Returns:
         Args:
     """
+    #Yolo Part
     parser = argparse.ArgumentParser()
     parser.add_argument('--weights', nargs='+', type=str, default='yolov7-tiny.pt', help='model.pt path(s)')
-    # parser.add_argument('--source', type=str, default='inference/images', help='source')  # file/folder, 0 for webcam
-    parser.add_argument('--source', type=str, default="2", help='source')
     parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
     parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
     parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
     parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
     parser.add_argument('--view-img', default='true', help='display results')
     parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
-    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
-    parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
     parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
     parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
     parser.add_argument('--augment', action='store_true', help='augmented inference')
-    parser.add_argument('--update', action='store_true', help='update all models')
-    parser.add_argument('--project', default='runs/detect', help='save results to project/name')
-    parser.add_argument('--name', default='exp', help='save results to project/name')
-    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
     parser.add_argument('--no-trace', action='store_true', help='don`t trace model')
+
     return parser.parse_args()
 
 
-# detect model init and get
+"""
+-------------------------------------
+    # detect model init and get
+-------------------------------------
+"""
 def plot_frame(pred, img1, img, names, colors):
     for i, det in enumerate(pred):  # detections per image
         gn = torch.tensor(img.shape)[[1, 0, 1, 0]]  # normalization gain whwh
@@ -80,6 +78,7 @@ def plot_frame(pred, img1, img, names, colors):
 
         cv2.imshow("Vision", img)
         cv2.waitKey(1)  # 1 millisecond
+
 
 def detect(img, model, imgsz, device, half, view_img, names, colors, stride):
     global old_img_b, old_img_h, old_img_w
@@ -118,11 +117,9 @@ def detect(img, model, imgsz, device, half, view_img, names, colors, stride):
 def init_model():
     global old_img_b, old_img_h, old_img_w
     # Init Yolo Model
-    source, weights, view_img, save_txt, imgsz, trace = args.source, args.weights, args.view_img, \
+    weights, view_img, save_txt, imgsz, trace = args.weights, args.view_img, \
         args.save_txt, args.img_size, not args.no_trace
     set_logging()
-    webcam = source.isnumeric() or source.endswith('.txt') or source.lower().startswith(
-        ('rtsp://', 'rtmp://', 'http://', 'https://'))
     device = select_device(args.device)
     half = device.type != 'cpu'
 
@@ -136,14 +133,6 @@ def init_model():
     if half:
         model.half()  # to FP16
 
-    vid_path, vid_writer = None, None
-    # if webcam:
-    #     view_img = check_imshow()
-    #     cudnn.benchmark = True  # set True to speed up constant image size inference
-    #     dataset = LoadStreams(source, img_size=imgsz, stride=stride)
-    # else:
-    #     dataset = LoadImages(source, img_size=imgsz, stride=stride)
-
     # Get names and colors
     names = model.module.names if hasattr(model, 'module') else model.names
     colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
@@ -156,15 +145,14 @@ def init_model():
 
     old_img_w = old_img_h = imgsz
     old_img_b = 1
-    return model, imgsz, device, half, view_img, webcam, names, colors, stride
+    return model, imgsz, device, half, view_img, names, colors, stride
+
 
 def callback(data1):
     global color_image
     bridge = CvBridge()
     color_image = bridge.imgmsg_to_cv2(data1, 'bgr8')
 
-    # cv2.imshow('color_image', color_image)
-    # cv2.waitKey(1)
 
 # def callback2(data2):
 #     global depth_image
@@ -176,7 +164,6 @@ def callback(data1):
 def callback3(data3):
     global basic
     basic = data3
-
 
 
 if __name__ == '__main__':
@@ -191,7 +178,7 @@ if __name__ == '__main__':
     r = rospy.Rate(10)  # 10Hz
 
     # Init Model
-    model, imgsz, device, half, view_img, webcam, names, colors, stride = init_model()
+    model, imgsz, device, half, view_img, names, colors, stride = init_model()
 
     while True:
         try:
@@ -207,5 +194,5 @@ if __name__ == '__main__':
             cv2.destroyAllWindows()
             rospy.signal_shutdown("shut_down")
             break
-            
+
     rospy.spin()
